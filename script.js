3a7d8d06708e234bb388772d625c89528b794eb2
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-24 01:18:12 +0200

    Third commit

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 const question = [
     'what is your favorite hobby?',
-    'how are you old?',
+    'How old are you?',
     'what is your name?',
-    'do you have a gf?',
+    'do you have a girlfriend?',
 ];
 
 let answers =[]
 
 function memories() {
 
-
     for ( let i = 0; i < question.length; i++ ){
 
-       answers[i] = prompt(question[i])
+       answers[i] = prompt(question[i]);
+
         if(answers[i] != '' && answers[i].length < 30){
             console.log('+');
         } else {
@@ -22,31 +22,42 @@ function memories() {
             console.log('-');
         }
     }
-
+    let hobby = document.getElementById('my_hobbies').innerHTML = `${answers[0]}`
+    let years = document.getElementById('my_years').innerHTML = `${answers[1]}`
+    let name = document.getElementById('my_name').innerHTML = `${answers[2]}`
+    let gf = document.getElementById('my_gf');
+    let my_gf = document.getElementById('gf_text');
+    
+    if(answers[3] === 'no' || answers[3] === 'No'){
+        gf.innerHTML = ``
+        my_gf.innerHTML = `I dont have girlfriend`
+    } else {
+        let gf_answer = prompt('What is your girlfriends name?')
+        gf.innerHTML = `${gf_answer}`
+        my_gf.innerHTML = `You girlfriend's name:`
+    }
+    text()
 return answers;
-
 }
-console.log(memories());
 
 function text() {
-    
+
+    let pets = document.getElementById('my_pets')
 
     let animal = ''
-    let animals = confirm('do you have animal?');
+    let animals = confirm('do you have pet(s)?');
+    let animals_answer = document.getElementById('pets_text')
+    if(animals == false) {
 
-    for(let i = 0; i < 1; i++) {
-
-        if(animals === true){
-            animal = prompt('Which animal you have?')
-
+        animals_answer.innerHTML = `You dont have pet(s)`
+        }else {
+            animals_answer.innerHTML = `You have:`
+            animal = prompt(`what pets you have?`)
+            pets.innerHTML = `${animal}`;
+            
         }
-            if(animal !== '') {
-                console.log('+');
-            } else {
-                console.log('-');
-                i--
-            }
     }
+memories();
 
-}
-text()
+
+
